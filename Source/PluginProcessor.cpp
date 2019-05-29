1ca75cf835f98e734877d8fc14047bcd99d4c1cc
Taylor Govers <tgovers1998@gmail.com>
2019-05-30 02:51:39 +1200

    First commit for Ethan

---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,351 @@
+/*
+ ==============================================================================
+ 
+ This file was auto-generated!
+ 
+ It contains the basic framework code for a JUCE plugin processor.
+ 
+ ==============================================================================
+ */
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//String SoftwareSummativeAudioProcessor::paramCutoff ("cutoff");
+//String SoftwareSummativeAudioProcessor::paramResonance ("resonance");
+
+//==============================================================================
+Angry_ayayaAudioProcessor::Angry_ayayaAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+: AudioProcessor (BusesProperties()
+#if ! JucePlugin_IsMidiEffect
+#if ! JucePlugin_IsSynth
+                  .withInput  ("Input",  AudioChannelSet::stereo(), true)
+#endif
+                  .withOutput ("Output", AudioChannelSet::stereo(), true)
+#endif
+                  ), mState(*this, &mUndoManager, "Summative", {
+    
+    std::make_unique<AudioParameterFloat>("cutoff", "Cutoff", 20.0f, 20000.0f, 600.0f),
+    std::make_unique<AudioParameterFloat>("resonance", "Resonance", 1.0f, 5.0f, 1.0f),
+    std::make_unique<AudioParameterFloat>("drive", "Drive", NormalisableRange<float>(0.0f, 1.0f, 0.0001), 0.01f),
+    std::make_unique<AudioParameterFloat>("range", "Range", NormalisableRange<float>(0.0f, 3000.0f, 0.0001), 0.01f),
+    std::make_unique<AudioParameterFloat>("blend", "Blend", NormalisableRange<float>(0.1f, 1.0f, 0.0001), 0.1f),
+    std::make_unique<AudioParameterFloat>("volume", "Volume", NormalisableRange<float>(0.0f, 3.0f, 0.0001), 0.01f),
+    
+    
+}), lowPassFilter(dsp::IIR::Coefficients<float>::makeLowPass(44100, 20000.0f, 0.1f))
+
+
+#endif
+{
+    
+    
+    //mState.addParameterListener(resonance, this);
+    
+    
+    /*addParameter (cutoff = new AudioParameterFloat ("cutoff",
+     "Cutoff",
+     20.0f,
+     20000.0f,
+     600.0f));
+     
+     addParameter (resonance = new AudioParameterFloat ("resonance",
+     "Resonance",
+     1.0f,
+     5.0f,
+     1.0f));*/
+    //treeState = new AudioProcessorValueTreeState(*this, nullptr);
+    //treeState.createAndAddParameter("cutoff", "Cutoff", "cutoff", cutoffRange, 600.0f, nullptr, nullptr);
+    
+    
+    //cutoffParameter = treeState.getRawParameterValue("cutoff");
+    //resParameter = treeState.getRawParameterValue("resonance");
+    
+    //NormalisableRange<float> cutoffRange (20.0f, 20000.0f);
+    //NormalisableRange<float> resRange (1.0f, 5.0f);
+    
+    /*addParameter (cutoff = new AudioParameterFloat ("cutoff", "Cutoff", NormalisableRange<float> (20.0f, 20000.0f), 600.0f, nullptr, nullptr));
+     addParameter (resonance = new AudioParameterFloat ("resonance", "Resonance", NormalisableRange<float> (1.0f, 5.0f), 1.0f, nullptr, nullptr));*/
+    
+    // IMPLEMENT THIS
+    //treeState::addParameter("cutoff", "Cutoff", "cutoff", cutoffRange, 600.0f/*, nullptr, nullptr*/);
+    //treeState.createAndAddParameter("resonance", "Resonance", "resonance", resRange, 1.0f/*, nullptr, nullptr*/);
+    
+    //using Parameter = AudioProcessorValueTreeState::Parameter;
+    //createAndAddParameter (std::make_unique<Parameter> ("cutoff", "Cutoff", "cutoff", cutoffRange, 600.0f, nullptr, nullptr));
+    
+    //using Parameter2 = AudioProcessorValueTreeState::Parameter2;
+    //createAndAddParameter (std::make_unique<Parameter2> ("resonance", "Resonance", "resonance", resRange, 1.0f, nullptr, nullptr));
+}
+
+Angry_ayayaAudioProcessor::~Angry_ayayaAudioProcessor()
+{
+}
+
+//==============================================================================
+const String Angry_ayayaAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool Angry_ayayaAudioProcessor::acceptsMidi() const
+{
+#if JucePlugin_WantsMidiInput
+    return true;
+#else
+    return false;
+#endif
+}
+
+bool Angry_ayayaAudioProcessor::producesMidi() const
+{
+#if JucePlugin_ProducesMidiOutput
+    return true;
+#else
+    return false;
+#endif
+}
+
+bool Angry_ayayaAudioProcessor::isMidiEffect() const
+{
+#if JucePlugin_IsMidiEffect
+    return true;
+#else
+    return false;
+#endif
+}
+
+double Angry_ayayaAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int Angry_ayayaAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+    // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int Angry_ayayaAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void Angry_ayayaAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const String Angry_ayayaAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void Angry_ayayaAudioProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+//==============================================================================
+void Angry_ayayaAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+    
+    currentSampleRate = sampleRate;
+    //lastSampleRate = sampleRate;
+    
+    dsp::ProcessSpec spec;
+    spec.sampleRate = sampleRate;
+    spec.maximumBlockSize = samplesPerBlock;
+    spec.numChannels = getTotalNumInputChannels();
+    
+    lowPassFilter.prepare(spec);
+    lowPassFilter.reset();
+    
+    
+    //overdrive.prepare(spec);
+    //overdrive.reset();
+    
+    
+    // initiate mixLevel
+    //mixLevel.reset(sampleRate, 0.1f);
+    //mixLevel.setTargetValue(0.25f);
+    
+    
+}
+
+void Angry_ayayaAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool Angry_ayayaAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+#if JucePlugin_IsMidiEffect
+    ignoreUnused (layouts);
+    return true;
+#else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono()
+        && layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
+        return false;
+    
+    // This checks if the input layout matches the output layout
+#if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+#endif
+    
+    return true;
+#endif
+}
+#endif
+
+void Angry_ayayaAudioProcessor::updateFilter()
+{
+    float freq = *mState.getRawParameterValue("cutoff");
+    float res = *mState.getRawParameterValue("resonance");
+    
+    *lowPassFilter.state = *dsp::IIR::Coefficients<float>::makeLowPass(currentSampleRate, freq, res);
+}
+
+/*void SoftwareSummativeAudioProcessor::updateDistParameters()
+ {
+ if (sampleRate != 0.0)
+ {
+ overdrive.get<0>().setGainDecibels (static_cast<float> (inGainParam.getCurrentValue()));
+ overdrive.get<4>().setGainDecibels (static_cast<float> (outGainParam.getCurrentValue()));
+ }
+ }*/
+
+void Angry_ayayaAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiMessages)
+{
+    ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+    
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+    
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    // Make sure to reset the state if your inner loop is processing
+    // the samples and the outer loop is handling the channels.
+    // Alternatively, you can process the samples with the channels
+    // interleaved by keeping the same state.
+    
+    //buffer.applyCutoff (*cutoff);
+    //buffer.applyResonance (*resonance);
+    
+    dsp::AudioBlock<float> block (buffer);
+    
+    updateFilter();
+    lowPassFilter.process(dsp::ProcessContextReplacing <float> (block));
+    
+    //updateDistParameters();
+    //overdrive.process(dsp::ProcessContextReplacing <float> (block));
+    
+    float mDrive = *mState.getRawParameterValue("drive");
+    float mRange = *mState.getRawParameterValue("range");
+    float mBlend = *mState.getRawParameterValue("blend");
+    float mVolume = *mState.getRawParameterValue("volume");
+    
+    AudioBuffer<float> wetBuffer(getTotalNumInputChannels(), buffer.getNumSamples());
+    wetBuffer.makeCopyOf(buffer);
+    
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        //auto* channelData = buffer.getWritePointer (channel);
+        //auto* wetData = wetBuffer.getWritePointer (channel);
+        
+        // ..do something to the data...
+        
+        auto* channelData = buffer.getWritePointer(channel);
+        
+        
+        for(int sample = 0; sample < buffer.getNumSamples(); ++sample)
+        {
+            //updateFilter();
+            //lowPassFilter.process(dsp::ProcessContextReplacing <float> (block));
+            
+            float cleanSig = *channelData;
+            
+            *channelData *= mDrive * mRange;
+            
+            *channelData = (((((2.0f / float_Pi) * atan(*channelData)) * mBlend) + (cleanSig * (1.0f / mBlend))) / 2) * mVolume;
+            
+            
+            channelData++;
+            
+            
+        }
+    }
+}
+
+//==============================================================================
+bool Angry_ayayaAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+AudioProcessorEditor* Angry_ayayaAudioProcessor::createEditor()
+{
+    return new Angry_ayayaAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void Angry_ayayaAudioProcessor::getStateInformation (MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+    
+    MemoryOutputStream stream(destData, false);
+    mState.state.writeToStream(stream);
+    
+}
+
+void Angry_ayayaAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+    
+    
+    ValueTree tree = ValueTree::readFromData(data, sizeInBytes);
+    if (tree.isValid()) {
+        mState.state = tree;
+    }
+    
+    
+    
+    
+    /*std::unique_ptr<XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
+     
+     if (xmlState.get() != nullptr)
+     {
+     if (xmlState->hasTagName ("SoftwareSummative"))
+     {
+     *cutoff = (float) xmlState->getDoubleAttribute ("cutoff", 600.0);
+     *resonance = xmlState->getDoubleAttribute ("resonance", 1.0f); // [5]
+     }
+     }*/
+    
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new Angry_ayayaAudioProcessor();
+}
+
---
+++ Source/PluginProcessor.h
@@ -0,0 +1,118 @@
+/*
+ ==============================================================================
+ 
+ This file was auto-generated!
+ 
+ It contains the basic framework code for a JUCE plugin processor.
+ 
+ ==============================================================================
+ */
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/**
+ */
+class Angry_ayayaAudioProcessor  : public AudioProcessor/*, public AudioProcessorValueTreeState::Listener*/
+
+{
+public:
+    //==============================================================================
+    Angry_ayayaAudioProcessor();
+    ~Angry_ayayaAudioProcessor();
+    
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+    
+#ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+#endif
+    
+    void processBlock (AudioBuffer<float>& buffer, MidiBuffer&) override;
+    
+    
+    //==============================================================================
+    AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+    
+    //==============================================================================
+    const String getName() const override;
+    
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+    
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const String getProgramName (int index) override;
+    void changeProgramName (int index, const String& newName) override;
+    
+    //==============================================================================
+    void getStateInformation (MemoryBlock& destData) override;
+    
+    
+    void setStateInformation (const void* data, int sizeInBytes) override;
+    
+    AudioProcessorValueTreeState& getValueTreeState();
+    
+    //static String paramCutoff;
+    //static String paramResonance;
+    
+    void updateFilter();
+    
+    void updateParameters();
+    
+    //void updateDistParameters();
+    
+    SmoothedValue<float>mixLevel;
+    //SmoothedValue<float>freqLevel;
+    //SmoothedValue<float>resLevel;
+    
+    //AudioProcessorValueTreeState treeState;
+    AudioProcessorValueTreeState mState;
+    
+private:
+    
+    float currentSampleRate;
+    //float lastSampleRate;
+    
+    
+    
+    //Atomic<float> cutoff { 600.0 };
+    // Atomic<float> resonance { 1.0 };
+    
+    AudioParameterFloat* cutoff;
+    AudioParameterFloat* resonance;
+    
+    AudioParameterFloat* drive;
+    AudioParameterFloat* range;
+    AudioParameterFloat* blend;
+    AudioParameterFloat* volume;
+    
+    float*driveParameter = nullptr;
+    float*rangeParameter = nullptr;
+    float*blendParameter = nullptr;
+    float*volumeParameter = nullptr;
+    
+    float*cutoffParameter = nullptr;
+    float*resParameter = nullptr;
+    
+    UndoManager mUndoManager;
+    
+    
+    Random random;
+    
+    dsp::ProcessorDuplicator<dsp::IIR::Filter <float>, dsp::IIR::Coefficients<float>> lowPassFilter;
+    
+    //dsp::ProcessorDuplicator<dsp::
+    
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Angry_ayayaAudioProcessor)
+};
+
